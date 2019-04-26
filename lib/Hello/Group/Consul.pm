06b22d835b48d1a9e9f2521c14b4ca823c6a4fbd
Rob N ★ <robn@robn.io>
2019-04-26 17:15:51 +1000

    Include service port in tester config if available

--- lib/Hello/Group/Consul.pm
+++ lib/Hello/Group/Consul.pm
@@ -187,6 +187,7 @@ sub _update_testers {
 
       my %member_config = %$config;
       $member_config{ip} = $service->service_address || $service->address;
+      $member_config{port} = $service->port if $service->port;
 
       my $kv_config =
         $service_args->{$service->node}->{$service->id} //
